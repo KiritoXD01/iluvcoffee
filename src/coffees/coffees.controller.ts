c408cc02b0adc51ac847352fee8bc0221c4c6ffa
Javier Emmanuel Mercedes <manuelmercedez10@gmail.com>
2023-03-10 18:11:50 -0400

    Controllers

---
+++ src/coffees/coffees.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Get } from '@nestjs/common';
+
+@Controller('coffees')
+export class CoffeesController {
+    @Get()
+    findAll() {
+        return 'This action returns all coffees';
+    }
+
+    @Get('/flavors')
+    findFlavors() {
+        return 'This action returns all coffees';
+    }
+}
